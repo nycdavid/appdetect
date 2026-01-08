cd3c24abb09a2e50898b07d11ab83d795a65f1f6
David Ko <davidsj.ko@gmail.com>
2026-01-08 06:03:52 -0500

    comments

--- Sources/appdetect/appdetect.swift
+++ Sources/appdetect/appdetect.swift
@@ -4,6 +4,11 @@
 @main
 struct appdetect {
     static func main() {
+        // 1. listen on a socket
+        // 2. request handling
+        //  - request comes in -> focused app is queried
+        //  - app information + metadata (as JSON) is returned via socket
+        //
         print("Hello, world!")
     }
 }
